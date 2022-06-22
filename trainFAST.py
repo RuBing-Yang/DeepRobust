242741764113e2477752735655b9465e2df1355c
RuBing-Yang <65957422+RuBing-Yang@users.noreply.github.com>
2022-06-23 06:06:22 +0800

    resnet的block是多少啊

--- trainFAST.py
+++ trainFAST.py
@@ -2,7 +2,7 @@ from deeprobust.image.defense.pgdtraining import PGDtraining
 from deeprobust.image.defense.fast import Fast
 
 from deeprobust.image.config import defense_params
-from deeprobust.image.netmodels.CNN import Net
+from deeprobust.image.netmodels.resnet import Net
 import torch
 from torchvision import datasets, transforms 
 
@@ -10,17 +10,19 @@ from torch.utils.data import DataLoader
 import setupData
 
 if __name__ == '__main__':
-    model = Net()
+    model = Net(block=1000, num_blocks=16, num_classes=20)
     train_data = setupData.CustomImageDataset(
         'train_phase1/label.csv', 
         'train_phase1/images/',
         transform = transforms.Compose([
-            transforms.Resize((500, 500)),
+            transforms.Resize((64, 64)),
+            transforms.ConvertImageDtype(torch.float),
+            transforms.Grayscale(num_output_channels=3),
             #transforms.ToTensor(),
         ]))
 
-    train_loader = DataLoader(train_data, batch_size=128, shuffle=True)
-    test_loader = DataLoader(train_data, batch_size=128, shuffle=True)
+    train_loader = DataLoader(train_data, batch_size=16, shuffle=True)
+    test_loader = DataLoader(train_data, batch_size=16, shuffle=True)
 
     # train_loader = torch.utils.data.DataLoader(
     #                 datasets.MNIST('deeprobust/image/defense/data', train=True, download=True,
@@ -34,5 +36,5 @@ if __name__ == '__main__':
     # defense = PGDtraining(model, 'cuda')
     # defense.generate(train_loader, test_loader, **defense_params["PGDtraining_MNIST"])
     defense = Fast(model, 'cuda')
-    defense.generate(train_loader, test_loader, **defense_params["FAST_MNIST"])
+    defense.generate(train_loader, test_loader) #, **defense_params["FAST_MNIST"])
 

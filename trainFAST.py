2c26c7b4385d49cec6043540ba540cd76fe1be96
RuBing-Yang <65957422+RuBing-Yang@users.noreply.github.com>
2022-06-24 18:10:12 +0800

    resnet

--- trainFAST.py
+++ trainFAST.py
@@ -1,6 +1,8 @@
 from deeprobust.image.defense.fast import Fast
 from deeprobust.image.config import defense_params
-import deeprobust.image.netmodels.resnet as resnet
+
+import resnet
+# import deeprobust.image.netmodels.resnet as resnet
 
 import torch
 from torchvision import datasets, transforms 
@@ -14,14 +16,14 @@ if __name__ == '__main__':
         'train_phase1/label.csv', 
         'train_phase1/images/',
         transform = transforms.Compose([
-            transforms.Resize([224, 224]),
+            transforms.Resize([128, 128]),
             transforms.ConvertImageDtype(torch.float),
             #transforms.Grayscale(num_output_channels=3),
             #transforms.ToTensor(),
-        ]))
+        ]))#
 
-    train_loader = DataLoader(clean_train_data, batch_size=64, shuffle=True)
-    test_loader = DataLoader(clean_train_data, batch_size=64, shuffle=True)
+    train_loader = DataLoader(clean_train_data, batch_size=2, shuffle=True)
+    test_loader = DataLoader(clean_train_data, batch_size=2, shuffle=True)
 
     # resnet50
     train_net = resnet.Net(resnet.Bottleneck, [3,8,36,3], num_classes=20)
